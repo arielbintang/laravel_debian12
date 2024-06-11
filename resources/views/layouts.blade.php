3d8b5c8f60ce51c321446f0e36accf9605e1e021
arielbintang <arielbintang78@gmail.com>
2024-06-11 12:43:43 +0700

    route ke dashboard

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -42,12 +42,13 @@
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="#">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+            <!-- Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
@@ -67,12 +68,11 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="">Buttons</a>
                         <a href="{{ route('barang.index') }}" class="collapse-item" href="cards.html">Barang</a>
                         <a href="{{ route('kategori.index') }}" class="collapse-item" href="cards.html">Kategori</a>
                         <a href="{{ route('barangmasuk.index') }}" class="collapse-item" href="cards.html">Barang Masuk</a>
                         <a href="{{ route('barangkeluar.index') }}" class="collapse-item" href="cards.html">Barang Keluar</a>
-                        <a class="collapse-item" href="">Cards</a>
+                        <!-- <a class="collapse-item" href="">Cards</a> -->
                     </div>
                 </div>
             </li>
